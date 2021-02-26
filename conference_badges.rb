4cfc1da5dc2dca14fdaee822d4043fc092f549fd
maryamzain123 <smaryam.zain@gmail.com>
2021-02-25 22:10:43 -0600

    commit tree 4937e89d82ca7863ae8a6c8cec3c41246ed012cd

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+
+def badge_maker(name)
+   return  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(people)
+    people.map do | greeting |
+        "Hello, my name is #{greeting}."
+    end
+end
+
+def assign_rooms(people)
+    assign_room = []
+    people.each.with_index(1) do | speaker, room |
+        assign_room.push "Hello, #{speaker}! You'll be assigned to room #{room}!"
+    end
+    return assign_room
+end
+
+def printer(people)
+    batch_badge_creator(people).each do |badge|
+    puts badge
+    end
+    assign_rooms(people).each do |room|
+    puts room
+    end
+end
+
+    
+
+
